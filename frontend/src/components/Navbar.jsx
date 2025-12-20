1856d797ed7a4a0066fb3e2541b1115ee4424ec9
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-12-21 02:25:11 +0530

    completed the navbar with responsive design

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/frontend_assets/assets.js'
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const [visible,setVisible] = useState(false);
+
+
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
         <img src={assets.logo} className='w-36' alt="" />
@@ -48,6 +52,22 @@ const Navbar = () => {
                     10
                 </p> 
             </Link>
+            <img onClick={()=> setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="menu_icon" />
+        </div>
+        {/*Sidebar menu for small screen */}
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'} `}>
+            <div className='flex flex-col text-gray-600'>
+                <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+
+                     <img src={assets.dropdown_icon} className='h-4 rotate-180' alt="dropdown_icon" />
+                     <p>Back</p>   
+                    {/* <p className='text-2xl '>X</p>  also add these in theonclick div justify-end px-3 py-5*/}
+                </div>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border-b border-gray-300' to='/'>HOME</NavLink>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border-b border-gray-300' to='/collection'>COLLECTION</NavLink>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border-b border-gray-300' to='/about'>ABOUT</NavLink>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border-b border-gray-300' to='/contact'>CONTACT</NavLink>
+            </div>
         </div>
     </div>
   )
