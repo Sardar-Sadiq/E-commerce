7e76a5e5d6b76f45d1c64183526cfb50f5ee218d
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-12-21 23:18:41 +0530

    working on best sellers

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -2,37 +2,35 @@ import React, { useContext, useEffect, useState } from 'react'
 
 import { ShopContext } from '../context/ShopContext.js'
 import Title from './Title.jsx';
-import ProductItem from './productItem.jsx';
+import ProductItem from './ProductItem.jsx';
 
 
-const LatestCollection = () => {
+function LatestCollection() {
 
     const { products } = useContext(ShopContext);
     const [latestProducts, setLatestProducts] = useState([]);
 
-    useEffect(()=> {
-        setLatestProducts(products.slice(0,10));
-    }, [products])
-    
-  return (
-    <div className='my-10'>
-        <div className='py-8 text-3xl text-center'>
-            <Title text1={'LATEST'} text2={'COLLECTIONS'} />
-            <p className='w-3/4 m-auto text-xs text-gray-600 sm:text-sm md:text-base'>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero nihil voluptatem modi quidem soluta asperiores odio, illo porro quia illum iure
-            </p>
-        </div>
+    useEffect(() => {
+        setLatestProducts(products.slice(0, 10));
+    }, [products]);
 
-        {/* rendring products */}
-        <div className='grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-6 '>
-            {
-                latestProducts.map((item,index)=>(
+    return (
+        <div className='my-10'>
+            <div className='py-8 text-3xl text-center'>
+                <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+                <p className='w-3/4 m-auto text-xs text-gray-600 sm:text-sm md:text-base'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero nihil voluptatem modi quidem soluta asperiores odio, illo porro quia illum iure
+                </p>
+            </div>
+
+            {/* rendring products */}
+            <div className='grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-6 '>
+                {latestProducts.map((item, index) => (
                     <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
-                ))
-            }
+                ))}
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
 export default LatestCollection
