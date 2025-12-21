e7cf139e43bd7d88952f1477e4ce04b3bbab462a
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-12-22 01:50:04 +0530

    completed the newsletter box compo, workin on footer

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 
 const NewsletterBox = () => {
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        //preventing the page refresh
+    }
+
   return (
     <div className='text-center '>
 
         <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
-      
+
+        <p className='mt-3 text-gray-400'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        </p>
+        <form onSubmit={onSubmitHandler} className='flex items-center w-full gap-3 pl-3 mx-auto my-6 border sm:w-1/2'>
+            <input className='w-full outline-none sm:flex-1' type="email" placeholder='Enter your email' required/>
+            <button type="submit" className='px-10 py-4 text-xs text-white bg-black'>SUBSCRIBE</button>
+        </form>
     </div>
   )
 }
 
-export default NewsletterBox
+export default NewsletterBox;
