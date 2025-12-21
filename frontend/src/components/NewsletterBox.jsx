303bb82e50de6287e14a41152c0dc7e7304c4de8
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-12-22 02:07:34 +0530

    completed teh UI part of Home page

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const NewsletterBox = () => {
 
     const onSubmitHandler = (event) => {
@@ -15,7 +16,7 @@ const NewsletterBox = () => {
         <p className='mt-3 text-gray-400'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
         </p>
-        <form onSubmit={onSubmitHandler} className='flex items-center w-full gap-3 pl-3 mx-auto my-6 border sm:w-1/2'>
+        <form onSubmit={onSubmitHandler} className='flex items-center w-full gap-3 pl-3 mx-auto my-6 border border-gray-200 sm:w-1/2'>
             <input className='w-full outline-none sm:flex-1' type="email" placeholder='Enter your email' required/>
             <button type="submit" className='px-10 py-4 text-xs text-white bg-black'>SUBSCRIBE</button>
         </form>
