807c16e805ccef8bae45b97a5467478be7a807bf
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-12-22 15:28:07 +0530

    working on collection page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,14 +1,34 @@
-import React, { useContext, useState } from 'react'
+import React, {  useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext.jsx'
 import { assets } from '../assets/frontend_assets/assets'
 import ProductItem from '../components/ProductItem.jsx'
+import Title from '../components/Title.jsx'
 
 const Collection = () => {
 
 const { products } = useContext(ShopContext);
 const [showFilter, setShowFilter] = useState(false);
+const [filterProducts, setFilterProducts] = useState([]);
+const [category,setCategory] = useState([]);
+const [subCategory, setSubCategory] = useState([]);
 
-console.log(products);
+const toggleCategory = (e) => {
+  if (category.includes(e.target.value)) {
+     setCategory(prev=> prev.filter(item => item !== e.target.value));
+  }
+  else {
+    setCategory(prev => [...prev, e.target.value])
+  }
+}
+
+useEffect(()=> {
+  setFilterProducts(products);
+},[products])
+
+useEffect(()=> {
+  console.log(category);
+},[category])
+
   return (
     <div className='flex flex-col gap-1 pt-10 border-t border-gray-200 sm:flex-row sm:gap-10 '>
       {/* Filter Options */}
@@ -22,13 +42,13 @@ console.log(products);
             <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
             <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
                 <p className='flex gap-2'>
-                  <input className='w-3' type="checkbox" value={'Men'} /> Men
+                  <input className='w-3' type="checkbox" value={'Men'} onChange={toggleCategory} /> Men
                 </p>
                 <p className='flex gap-2'>
-                  <input className='w-3' type="checkbox" value={'Women'} /> Women
+                  <input className='w-3' type="checkbox" value={'Women'} onChange={toggleCategory} /> Women
                 </p>
                 <p className='flex gap-2'>
-                  <input className='w-3' type="checkbox" value={'Kids'} /> Kids
+                  <input className='w-3' type="checkbox" value={'Kids'} onChange={toggleCategory} /> Kids
                 </p>
             </div>
         </div>
@@ -48,7 +68,28 @@ console.log(products);
                 </p>
             </div>
         </div>
+      </div>
+      {/* Right side Products Display */}
+      <div className='flex-1'>
+        
+        <div className='flex justify-between mb-4 text-base sm:text-2xl'>
+            <Title text1={'ALL'} text2={'COLLECTIONS'} />
+            {/* Sort Options */}
+            <select className='px-2 text-sm border-2 border-gray-300 '>
+              <option value="relevant">Sort by: Relevant</option>
+              <option value="low-high">Sort by: Low to High</option>
+              <option value="high-low">Sort by: High to Low</option>
+            </select>
+        </div>
 
+        {/* Map Products */}
+        <div className='grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 gap-y-6'>
+          {
+            filterProducts.map((item,index)=>(
+                <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image} />
+            ))
+          }
+        </div>
 
       </div>
 
