5499308ce6032fb4e378fad8d1ce36743492ae6a
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-12-24 16:27:57 +0530

    working on Products page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -31,10 +31,10 @@ const Product = () => {
   return productData ?  (
     <div className='pt-10 transition-opacity duration-500 ease-in border-t-2 border-gray-200 opacity-100'>
       {/*--------------------------- Product Data---------------------------- */}
-      <div className='flex flex-col gap-12 sm:gap-12 sm:flex-row'>
+      <div className='flex flex-col gap-12 sm:flex-row'>
         {/* ------------------------------ Product Images ---------------------------------*/}
         <div className='flex flex-col-reverse flex-1 gap-3 sm:flex-row'>
-          <div className='flex justify-between overflow-x-auto sm:flex-col sm:overflow-y-scroll sm:justify-normal sm:w-[18.7%] w-full'>
+          <div className='flex justify-between overflow-x-auto sm:flex-col sm:overflow-y-auto sm:justify-normal sm:w-[18.7%] w-full'>
             {
               productData.image.map((item,index)=> (
                 <img onClick={()=>setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 shrink-0 cursor-pointer'  alt="product_image" />
@@ -47,15 +47,15 @@ const Product = () => {
         </div>
 
         {/* -------------Product Info---------------------- */}
-        <div className=''>
-            <div className='flex-1'>
+      <div className=''>
+         <div className='flex-1'>
               <h1 className='mt-2 text-2xl font-medium'>{productData.name}</h1>
               <div className='flex items-center gap-1 mt-2'>
-                <img src={assets.star_icon} alt="" className="w-3 5" />
-                <img src={assets.star_icon} alt="" className="w-3 5" />
-                <img src={assets.star_icon} alt="" className="w-3 5" />
-                <img src={assets.star_icon} alt="" className="w-3 5" />
-                <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+                <img src={assets.star_icon} alt="" className="w-3.5" />
+                <img src={assets.star_icon} alt="" className="w-3.5" />
+                <img src={assets.star_icon} alt="" className="w-3.5" />
+                <img src={assets.star_icon} alt="" className="w-3.5" />
+                <img src={assets.star_dull_icon} alt="" className="w-3.5" />
                 <p className='pl-2'>(122)</p>
               </div>
               <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
@@ -76,10 +76,18 @@ const Product = () => {
                     <p>Cash on delivery is available on this product.</p>
                     <p>Easy return and exchange policy within 7 days.</p>
               </div>
+              
+            </div>
+      </div>
+      </div>
+
+      {/*--------------Description & Review Section------------ */}
+        <div className='mt-20'>
+            <div className='flex '>
+                  <b className='px-5 py-3 text-sm border border-gray-200'>Description</b>
+                  <p className='px-5 py-3 text-sm border border-gray-200 '>Reviews (122)</p>
             </div>
         </div>
-
-      </div>
     </div>
   ) : <div className='opacity-0' />
 }
